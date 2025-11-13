4b9b2c61d53113d71589350cc53b16e4f0f6d46d
焱铭 <lxb-yanming@foxmail.com>
2025-11-13 22:12:56 +0800

    去冗余

--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pyinstaller --onefile --icon=src/assets/icon.ico --name=PHE_FlipOptimizer src/main.py
-echo.
-echo 打包完成！
-pause
--- build.sh
+++
@@ -1,2 +0,0 @@
-pyinstaller --onefile --icon=src/assets/icon.ico --name=PHE_FlipOptimizer src/main.py
-echo "打包完成！"
